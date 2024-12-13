ef3100c6dc9d36cb1fe4f412b430c2df4f1775e1
Miquel Serra Llobera <misello@gmail.com>
2024-12-13 13:39:34 +0100

    Added day 3 2024 part 1

---
+++ 2024/Day_03/part1.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	//"strconv"
+)
+
+type tStatus int
+
+const (
+	Letter_m tStatus = iota
+	Letter_u
+	Letter_l
+	OpenParenthesis
+	Number_1
+	Comma
+	Number_2
+	CloseParenthesis
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please, provide just one file to analize.")
+		os.Exit(0)
+	}
+	fmt.Println("Opening file", os.Args[1])
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	fmt.Println("File", os.Args[1], "opened")
+
+	reader := bufio.NewReader(f)
+	var status tStatus
+	var num1, num2, sum int
+	var found bool
+
+	// Load input
+	fmt.Println("Reading character by character ...")
+	for {
+		char, err := reader.ReadByte()
+		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Println("End Of File reached!")
+				break
+			}
+			fmt.Println(err.Error())
+			os.Exit((1))
+		}
+		found = process(&status, &num1, &num2, char)
+		if found {
+			sum += num1 * num2
+		}
+	}
+
+	// Result
+	fmt.Printf("The sum of all the results of the valid multiplications is \033[1m%d\033[0m.\n", sum)
+}
+
+func process(s *tStatus, n1, n2 *int, c byte) bool {
+
+	fmt.Printf("%s", string(c))
+
+	switch *s {
+	case Letter_m:
+		if c == 'm' {
+			*s = Letter_u
+		}
+		return false
+	case Letter_u:
+		if c == 'u' {
+			*s = Letter_l
+			return false
+		} else if c == 'm' {
+			*s = Letter_u // keep the state
+			return false
+		}
+		*s = Letter_m
+		return false
+	case Letter_l:
+		if c == 'l' {
+			*s = OpenParenthesis
+			return false
+		} else if c == 'm' {
+			*s = Letter_u
+			return false
+		}
+		*s = Letter_m
+		return false
+	case OpenParenthesis:
+		if c == '(' {
+			*n1 = 0
+			*n2 = 0
+			*s = Number_1
+			return false
+		} else if c == 'm' {
+			*s = Letter_u
+			return false
+		}
+		*s = Letter_m
+		return false
+	case Number_1:
+		if c >= '0' && c <= '9' {
+			*n1 = *n1*10 + int(c-'0')
+			return false
+		} else if c == ',' && *n1 > 0 && *n1 < 1000 {
+			*s = Number_2
+			return false
+		} else if c == 'm' {
+			*s = Letter_u
+			return false
+		}
+		*s = Letter_m
+		return false
+	case Comma:
+		// Not used
+	case Number_2:
+		if c >= '0' && c <= '9' {
+			*n2 = *n2*10 + int(c-'0')
+			return false
+		} else if c == ')' && *n2 > 0 && *n2 < 1000 {
+			*s = Letter_m
+			fmt.Printf(" --> %d x %d = %d\n", *n1, *n2, *n1**n2)
+			return true
+		} else if c == 'm' {
+			*s = Letter_u
+			return false
+		}
+		*s = Letter_m
+		return false
+	case CloseParenthesis:
+		// Not used
+	}
+	return false
+}
