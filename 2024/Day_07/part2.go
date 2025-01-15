e495d5b8e1df2de7a370cc1f267e24e24a42160c
Miquel Serra Llobera <misello@gmail.com>
2025-01-15 09:03:34 +0100

    Added day 7 2024 part 2

---
+++ 2024/Day_07/part2.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please, provide just one file to analize.")
+		os.Exit(0)
+	}
+	fmt.Println("Opening file", os.Args[1])
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	fmt.Println("File", os.Args[1], "opened")
+
+	scn := bufio.NewScanner(f)
+	sum := 0
+
+	// Check te equations one by one
+	fmt.Println("Checking equations ...")
+	for scn.Scan() {
+		eq := scn.Text()
+		solStr, numStr, found := strings.Cut(eq, ": ")
+		if !found {
+			panic(fmt.Errorf("Separator \": \" not found on equation \"%s\"", eq))
+		}
+
+		sol, err := strconv.Atoi(solStr)
+		if err != nil {
+			panic(err)
+		}
+
+		numStrSlice := strings.Split(numStr, " ")
+
+		if anySolution(sol, numStrSlice, []int{}) {
+			fmt.Println(" Solution found on", numStr, "=", sol, "☺")
+			sum += sol
+		} else {
+			fmt.Println(" No solution found on", numStr, "=", sol)
+		}
+	}
+
+	// Result
+	fmt.Printf("The total calibration result is \033[1m%d\033[0m.\n", sum)
+}
+
+// anySolution returns true if adding "+" and "*" between the numbers in ns
+// we can find the result r
+// This is done using the temporary results tmp
+func anySolution(r int, ns []string, tmp []int) bool {
+
+	// No initialized numbers slice, error.
+	if ns == nil {
+		panic(fmt.Errorf("Slice passed to anySolution is empty or nil"))
+	}
+
+	// No more numbers, check if one is the result
+	if len(ns) == 0 {
+		for _, x := range tmp {
+			if x == r {
+				return true
+			}
+		}
+		return false
+	}
+
+	// Operate next number
+	n, err := strconv.Atoi(ns[0])
+	if err != nil {
+		panic(err)
+	}
+
+	// We create the new tmp slice of results
+	var newTmp []int
+	if len(tmp) == 0 {
+		newTmp = append(newTmp, n)
+	} else {
+		for _, x := range tmp {
+			newX1 := x + n
+			newX2 := x * n
+			newX3 := concat(x, n)
+			newTmp = append(newTmp, newX1, newX2, newX3)
+		}
+	}
+
+	return anySolution(r, ns[1:], newTmp)
+
+}
+
+// concat takes 2 numbers and return the result of adding both together
+// concat(5, 3) = 53
+func concat(a, b int) int {
+	as := strconv.Itoa(a)
+	bs := strconv.Itoa(b)
+	cs := as + bs
+	c, err := strconv.Atoi(cs)
+	if err != nil {
+		panic(err)
+	}
+	return c
+}
