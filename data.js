e8dbb80fc39a7df88e582d524c27010e8d68c89f
Miquel Serra Llobera <misello@gmail.com>
2025-03-21 11:01:58 +0100

    Added the slots for years 2023 & 2024

--- data.js
+++ data.js
@@ -1,13 +1,157 @@
 const texts = {
-    "2023": {
-        "1": { part1: "Text for Part 1 - Day 1, 2023", part2: "Text for Part 2 - Day 1, 2023" },
-        "2": { part1: "Text for Part 1 - Day 2, 2023", part2: "Text for Part 2 - Day 2, 2023" }
-    },
-    "2024": {
-        "1": { part1: "Text for Part 1 - Day 1, 2024", part2: "Text for Part 2 - Day 1, 2024" }
-    },
-    "XXXX": {
-        "x": { part1: "xx"}
-    }
+	"2023": {
+		"1" : {
+			part1: "",
+			part2: "" },
+		"2" : {
+			part1: "",
+			part2: "" },
+		"3" : {
+			part1: "",
+			part2: "" },
+		"4" : {
+			part1: "",
+			part2: "" },
+		"5" : {
+			part1: "",
+			part2: "" },
+		"6" : {
+			part1: "",
+			part2: "" },
+		"7" : {
+			part1: "",
+			part2: "" },
+		"8" : {
+			part1: "",
+			part2: "" },
+		"9" : {
+			part1: "",
+			part2: "" },
+		"10": {
+			part1: "",
+			part2: "" },
+		"11": {
+			part1: "",
+			part2: "" },
+		"12": {
+			part1: "",
+			part2: "" },
+		"13": {
+			part1: "",
+			part2: "" },
+		"14": {
+			part1: "",
+			part2: "" },
+		"15": {
+			part1: "",
+			part2: "" },
+		"16": {
+			part1: "",
+			part2: "" },
+		"17": {
+			part1: "",
+			part2: "" },
+		"18": {
+			part1: "",
+			part2: "" },
+		"19": {
+			part1: "",
+			part2: "" },
+		"20": {
+			part1: "",
+			part2: "" },
+		"21": {
+			part1: "",
+			part2: "" },
+		"22": {
+			part1: "",
+			part2: "" },
+		"23": {
+			part1: "",
+			part2: "" },
+		"24": {
+			part1: "",
+			part2: "" },
+		"25": {
+			part1: "",
+			part2: "" }
+	},
+	"2024": {
+		"1" : {
+			part1: "",
+			part2: "" },
+		"2" : {
+			part1: "",
+			part2: "" },
+		"3" : {
+			part1: "",
+			part2: "" },
+		"4" : {
+			part1: "",
+			part2: "" },
+		"5" : {
+			part1: "",
+			part2: "" },
+		"6" : {
+			part1: "",
+			part2: "" },
+		"7" : {
+			part1: "",
+			part2: "" },
+		"8" : {
+			part1: "",
+			part2: "" },
+		"9" : {
+			part1: "",
+			part2: "" },
+		"10": {
+			part1: "",
+			part2: "" },
+		"11": {
+			part1: "",
+			part2: "" },
+		"12": {
+			part1: "",
+			part2: "" },
+		"13": {
+			part1: "",
+			part2: "" },
+		"14": {
+			part1: "",
+			part2: "" },
+		"15": {
+			part1: "",
+			part2: "" },
+		"16": {
+			part1: "",
+			part2: "" },
+		"17": {
+			part1: "",
+			part2: "" },
+		"18": {
+			part1: "",
+			part2: "" },
+		"19": {
+			part1: "",
+			part2: "" },
+		"20": {
+			part1: "",
+			part2: "" },
+		"21": {
+			part1: "",
+			part2: "" },
+		"22": {
+			part1: "",
+			part2: "" },
+		"23": {
+			part1: "",
+			part2: "" },
+		"24": {
+			part1: "",
+			part2: "" },
+		"25": {
+			part1: "",
+			part2: "" }
+	}
 };
 
